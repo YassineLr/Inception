6e152442792970c5603dba977e14f24bbfff1ce7
YassineLr <yassine.lr.2001@gmail.com>
2024-04-05 21:45:29 +0000

    mariadb

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,3 +1,7 @@
 echo "CREATE DATABASE IF NOT EXISTS db_name" | mariadb
 echo "CREATE USER IF NOT EXISTS yassine IDENTIFIED BY '123'" | mariadb
 echo "GRANT ALL PRIVILEGES ON db_name.* TO 'yassine' IDENTIFIED BY '123'" | mariadb
+
+service mariadb stop
+
+mysqld_safe
