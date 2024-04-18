0665a0b082d5f9e4f9bac4d75b523e4239b71198
YassineLr <yassine.lr.2001@gmail.com>
2024-04-18 22:15:54 +0100

    mariadb dockerfile

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -2,6 +2,6 @@ echo "CREATE DATABASE IF NOT EXISTS db_name" | mariadb
 echo "CREATE USER IF NOT EXISTS yassine IDENTIFIED BY '123'" | mariadb
 echo "GRANT ALL PRIVILEGES ON db_name.* TO 'yassine' IDENTIFIED BY '123'" | mariadb
 
-service mariadb stop
+service mysql start
 
 mysqld_safe
